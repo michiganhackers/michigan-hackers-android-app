33152d7a2b83ed1890627d7bd94a96c651ae925d
Owain <owaink2255@gmail.com>
2018-07-21 05:36:46 -0400

    Implementing firebase notifications

---
+++ app/src/main/java/org/michiganhackers/michiganhackers/mFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package org.michiganhackers.michiganhackers;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class mFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FirebaseMessaging";
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        //TODO Handle FCM messages here
+        Log.d(TAG, "From: " + message.getFrom());
+        if (message.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + message.getNotification().getBody());
+        }
+    }
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+    }
+
+}
