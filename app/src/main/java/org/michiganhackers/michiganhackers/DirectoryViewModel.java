5bedb21e613a99fe978fe5014131a0e42ae4fb4d
Vincent Nagel <vnagel@umich.edu>
2018-08-04 22:23:41 -0400

    changed to childeventlistener

--- app/src/main/java/org/michiganhackers/michiganhackers/DirectoryViewModel.java
+++ app/src/main/java/org/michiganhackers/michiganhackers/DirectoryViewModel.java
@@ -3,7 +3,10 @@ package org.michiganhackers.michiganhackers;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,7 +18,7 @@ import java.util.TreeMap;
 
 public class DirectoryViewModel extends ViewModel{
     private MutableLiveData<Map<String, Team>> teamsByName;
-    private Map<String, Team> teamsByNameLocal; //TOdo add child event listener
+    private Map<String, Team> teamsByNameLocal;
     private DatabaseReference teamsRef = FirebaseDatabase.getInstance().getReference().child("Teams");
     public DirectoryViewModel(){
         teamsByNameLocal = new TreeMap<>();
@@ -24,21 +27,38 @@ public class DirectoryViewModel extends ViewModel{
         }
         else{
             teamsByName = new MutableLiveData<>();
-            teamsRef.addValueEventListener(new ValueEventListener() {
+            teamsRef.addChildEventListener(new ChildEventListener() {
                 @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        Team team = snapshot.getValue(Team.class);
-                        teamsByNameLocal.put(team.getName(), team);
-                    }
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Team team = dataSnapshot.getValue(Team.class);
+                    teamsByNameLocal.put(team.getName(), team);
                     teamsByName.setValue(teamsByNameLocal);
                 }
+
                 @Override
-                public void onCancelled(DatabaseError databaseError) {
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Team team = dataSnapshot.getValue(Team.class);
+                    teamsByNameLocal.put(team.getName(), team);
+                    teamsByName.setValue(teamsByNameLocal);
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                    Team team = dataSnapshot.getValue(Team.class);
+                    teamsByNameLocal.remove(team.getName());
+                    teamsByName.setValue(teamsByNameLocal);
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                     //Todo
                 }
-            });
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Todo
+                }
+            });
         }
     }
     LiveData<Map<String, Team>> getTeamsByName(){
