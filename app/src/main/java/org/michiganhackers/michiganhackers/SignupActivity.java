913e6b59d22d242e3178a162e4852d853c1b7f29
Vincent Nagel <vnagel@umich.edu>
2018-08-06 22:11:15 -0400

    remove signup toast message

--- app/src/main/java/org/michiganhackers/michiganhackers/SignupActivity.java
+++ app/src/main/java/org/michiganhackers/michiganhackers/SignupActivity.java
@@ -73,11 +73,7 @@ public class SignupActivity extends AppCompatActivity {
                         .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                Toast.makeText(SignupActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
                                 progressBar.setVisibility(View.GONE);
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
                                 if (!task.isSuccessful()) {
                                     Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
                                             Toast.LENGTH_SHORT).show();
